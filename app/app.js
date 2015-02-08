ffee9b31ee148a4a87a43063e5fe57b13ad953b0
Udbhav Sharma <udbhav.sharma15@gmail.com>
2015-02-08 11:51:25 +0530

    POC completed

---
+++ app/app.js
@@ -0,0 +1,180 @@
+var app = angular.module('exam',[]).
+    constant(
+        "CONSTANTS",{
+            SUCCESS_CODE: 1,
+            ERROR_CODE: 0,
+            NETWORK_ERROR: 'Some Error occurred! Please reload/refresh the page and try again.',
+            DELETE_CONFIRMATION: 'Do you really want to delete this registration?',
+            LOADING: 'Loading...'
+        }
+    );
+
+app.controller('sheet', function($scope, CONSTANTS, $http) {
+
+    //Variables
+
+    //Constants
+    $scope.marksPattern = new RegExp("^[0-9]+$|^[0-9]*[.][0-9]+$");
+    $scope.gradePattern = new RegExp("^[ABCDPF]$|^[E][X]$");
+    $scope.gradeType = [
+        {grade:'EX', minMarks:85},
+        {grade:'A', minMarks:70},
+        {grade:'B', minMarks:60},
+        {grade:'C', minMarks:50},
+        {grade:'D', minMarks:40},
+        {grade:'P', minMarks:33},
+        {grade:'F', minMarks:0}
+    ];
+
+    //Pre Initialized
+    $scope.exams= [
+        {examId:1, examName:'Minor 1', maxMarks:10},
+        {examId:2, examName:'Mid', maxMarks:30},
+        {examId:3, examName:'Minor 2', maxMarks:10},
+        {examId:4, examName:'End', maxMarks:50}
+    ];
+    $scope.rollNumbers = ['117161','117162','117163','117164','117165','117166','117167','117168','117169','117170'];
+
+    //Default Assignment
+    $scope.cells = initializeCells();
+    $scope.contribution = initializeContribution();
+    $scope.grades={};
+    $scope.avg = {};
+    $scope.total = {};
+
+    //Functions
+
+    //Initialization Functions
+    function initializeCells(){
+        var cells={};
+        for(var i in $scope.rollNumbers) {
+            cells[$scope.rollNumbers[i]] = {};
+            for (var j in $scope.exams)
+                cells[$scope.rollNumbers[i]][$scope.exams[j].examId] = defaultValue;
+        }
+        return cells;
+    }
+
+    function initializeContribution(){
+        var contribution={};
+        for (var i in $scope.exams)
+            contribution[$scope.exams[i].examId] = 1;
+        return contribution;
+    }
+
+    $scope.calculateAverageMarks = function calculateAverageMarks( column ){
+        var list = [];
+        for(var i in $scope.rollNumbers)
+            list.push($scope.cells[$scope.rollNumbers[i]][column]);
+
+        $scope.avg[column] = average(list);
+        return $scope.avg[column];
+    }
+
+    $scope.calculateColumnTotal = function calculateColumnTotal( arrayList ){
+        var list = [];
+        for(var i in arrayList)
+            list.push(arrayList[i]);
+
+        return sum(list);
+    }
+
+    $scope.calculateColumnContributionTotal = function calculateColumnContributionTotal( arrayList ){
+        var list = [];
+        var value;
+        for(var i in arrayList){
+            if(isFormatValid($scope.contribution[i]) && isFormatValid(arrayList[i]))
+                value=arrayList[i]*$scope.contribution[i];
+            else
+                value=defaultValue;
+
+            list.push(value);
+        }
+        return sum(list);
+    }
+
+    $scope.calculateTotalMarks = function calculateTotalMarks( roll, arrayList ) {
+        var totalMarks = $scope.calculateColumnContributionTotal(arrayList);
+        $scope.total[roll] = totalMarks;
+        return totalMarks;
+    }
+
+    $scope.calculateGrade = function calculateGrade(){
+        var finalGrade;
+        for(var j in $scope.total){
+            finalGrade=defaultValue;
+            for( var i in $scope.gradeType ){
+                if(!isFormatValid($scope.total[j]))
+                    break;
+                if(isFormatValid($scope.gradeType[i].minMarks) &&
+                    parseFloat($scope.total[j]) >= parseFloat($scope.gradeType[i].minMarks)){
+                    finalGrade = $scope.gradeType[i].grade;
+                    break;
+                }
+            }
+            $scope.grades[j]=finalGrade;
+        }
+        $scope.drawGraph();
+    }
+
+    $scope.drawGraph = function drawGraph(){
+        var gradeCount = getGradesCount();
+        console.log(gradeCount);
+        draw( gradeCount );
+    }
+
+    function getGradesCount(){
+        var gradeCount = [];
+        var i;
+        for( i in $scope.gradeType ) {
+            gradeCount.push({'grade':$scope.gradeType[i].grade, 'count':0 });
+        }
+
+        for( i in $scope.grades ){
+            for( var j in gradeCount ){
+                if(gradeCount[j].grade==$scope.grades[i])
+                    gradeCount[j].count++;
+            }
+        }
+        return gradeCount;
+    }
+
+    function sum( list ){
+        var tempSum = defaultValue;
+        for( var i in list)
+            if( isFormatValid(list[i]) )
+            {
+                if(!isFormatValid(tempSum))
+                    tempSum=0;
+                tempSum += parseFloat(list[i]);
+            }
+        return tempSum;
+    }
+
+    function average( list ){
+        var tempSum = sum( list );
+        if(isFormatValid(tempSum))
+            tempSum = tempSum/list.length;
+        return tempSum;
+    }
+
+    var defaultValue="";
+
+    function isFormatValid( value ){
+        if(value===defaultValue || angular.isUndefined( value ))
+            return false;
+        return true;
+    }
+
+});
+
+function draw( graphData ){
+    $('#graph').html('');
+    Morris.Bar({
+        element: 'graph',
+        data:graphData,
+        xkey: 'grade',
+        ykeys: ['count'],
+        labels: ['Grade Count']
+    });
+}
